79eaffb08165e6e814fc8cd4fd8b25d2d63871ad
JPV <jpvalencia@gmail.com>
2017-09-22 20:28:58 -0300

    settings

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,8 +1,8 @@
 export default {
   TOKEN_SECRET: process.env.TOKEN_SECRET || "token",
-  DB_USER: process.env.DB_USER || "valencia",
-  DB_PASSWORD: process.env.DB_PASSWORD || "cloudtorinobuffa1150",
-  DB_HOST: process.env.DB_HOST || "hipopo.cpnoxtjcjh9q.us-west-2.rds.amazonaws.com",
+  DB_USER: process.env.DB_USER || "tuto",
+  DB_PASSWORD: process.env.DB_PASSWORD || "tuto",
+  DB_HOST: process.env.DB_HOST || "localhost",
   DB_PORT: process.env.DB_PORT || "3306",
   DB_NAME: process.env.DB_NAME || "hipopo"
 }
